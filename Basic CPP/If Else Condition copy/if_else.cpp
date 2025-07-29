4b659b282349ec21996f84b523fdb569ebac2db0
Khorsed Alam <khorsedalam0472@gmail.com>
2025-07-29 23:44:32 +0600

    Basic CPP

---
+++ Basic CPP/If Else Condition copy/if_else.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+   
+    int n1;
+    int n2;
+    int n3;
+    int sum;
+    int sub;
+    cout<<"Enter the first Number: ";
+    cin>>n1;
+    cout<<"Enter the second Number: ";
+    cin>>n2;
+    cout<<"Enter the Third Number: ";
+    cin>>n3;
+
+    
+    if(n1>n2){
+        sum=n1+n2;
+        cout<<"After condition apply: "<<sum;
+    }
+    else{
+         sub=n1-n2;
+        cout<<"After Condition apply: "<<sub;
+    }
+
+
+}
