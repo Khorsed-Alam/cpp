c74412754daf7d91ba85a665709369b0f14b131a
Khorsed Alam <khorsedalam0472@gmail.com>
2025-07-29 22:18:16 +0600

    String

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,26 @@ using namespace std;
 int main()
 {
     cout<<"Hello"<< endl;
-    return 0;
+     
+    
+    string s;
+
+    cout<<"Enter a String: ";
+    getline(cin,s);
+
+    cout<< "You have entered : ";
+    cout<<s<<endl;
+
+    s.push_back('A');// add a single character into a string 
+    cout<<"after using push back: "<<s<<endl;
+
+    s.pop_back();
+    cout<<"after using pop back: "<<s<<endl;
+
+    s.push_start('M');
+    cout<<"After using push start: "<<s<<endl;
+
+
+
+
 }
