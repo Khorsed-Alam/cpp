ea225ce17ae972bbbcb03d625ab0f2408f297cbd
Khorsed Alam <khorsedalam0472@gmail.com>
2025-07-29 22:36:22 +0600

    For loop

---
+++ Loop/For_Loop.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter user input ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"* ";
+    }
+    return 0;
+}
